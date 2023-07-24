16ec0eb2068df5ade361729f34bc87bee0b993a4
NIPUN_OFFICIAL_28 <nipisht21@gmail.com>
2023-07-24 09:44:04 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      'custom': ['Helvetica', 'Arial', 'sans-serif'],
+    }
+  },
+  
+  plugins: [],
+}
